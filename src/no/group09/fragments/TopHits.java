eefb96a3b289b974f171d1b507c39f4d1c14edfa
Bjørn Arve Fossum <bjofoss2@hotmail.com>
2013-02-21 12:31:40 +0100

    Fix license files
     * Add UbiCollab as copyright holder
    
     * Replace license headers to conform with UbiCollab standard
    
     * Add NOTICE file
    
     * Close #12

--- src/no/group09/fragments/TopHits.java
+++ src/no/group09/fragments/TopHits.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to UbiCollab.org under one or more contributor
+ * license agreements.  See the NOTICE file distributed 
+ * with this work for additional information regarding
+ * copyright ownership. UbiCollab.org licenses this file
+ * to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package no.group09.fragments;
 
 import java.util.ArrayList;
@@ -18,19 +36,6 @@ import android.widget.RatingBar;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 public class TopHits extends MyFragment{
 
