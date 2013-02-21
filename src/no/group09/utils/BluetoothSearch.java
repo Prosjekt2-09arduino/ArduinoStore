e5e136246e66f9eea6c13ace96794dfadd91bd4d
Robin Tordly <robin.tordly@gmail.com>
2013-02-21 11:10:46 +0100

    commented out a line that was not in use

---
+++ src/no/group09/utils/BluetoothSearch.java
@@ -0,0 +1,29 @@
+package no.group09.utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+public class BluetoothSearch extends AsyncTask<Void, Void, String> {
+
+
+    public BluetoothSearch(Context context) {
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        return null;
+    }      
+
+    @Override
+    protected void onPostExecute(String result) {                           
+
+    }
+
+}
