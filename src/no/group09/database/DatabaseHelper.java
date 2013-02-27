85c6bb5c95351b7c545a5476c17ea79c9513c05a
Robin Tordly <robin.tordly@gmail.com>
2013-02-27 12:43:39 +0100

    Added a simple database, this is not fully functionally yet. Changing branch now

--- src/no/group09/database/DatabaseHelper.java
+++ src/no/group09/database/DatabaseHelper.java
@@ -1,106 +1,54 @@
 package no.group09.database;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
 	private static final String DATABASE_NAME = "uCSoftwareStore";
-
+	private static final String DATABASE_PATH = "/data/data/YOUR_PACKAGE/databases/";
 	private static final int DATABASE_VERSION = 2;
+	private Context myContext;
+	private SQLiteDatabase myDataBase = null;
 
-	// Database app creation sql statement
-	private static final String DATABASE_CREATE_APP = 
-			"CREATE TABLE app (" +
-					"appid integer primary key autoincrement, " +
-					"name varchar(160), " +
-					"description varchar(200), " +
-					"developerid int(10), " +
-					"image BLOB)";
-
-	// Database version creation sql statement
-	private static final String DATABASE_CREATE_VERSION = 
-			"CREATE TABLE version (" +
-					"versionid integer primary key autoincrement, " +
-					"version varchar(160), " +
-					"fileURL varchar(200), " +
-					"filesize int(10))";
-
-	// Database ratings creation sql statement
-	private static final String DATABASE_CREATE_RATINGS = 
-			"CREATE TABLE ratings (" +
-					"ratingid integer primary key autoincrement, " +
-					"title varchar(160), " +
-					"comment varchar(200), " +
-					"rating int(10))";
-	
-	// Database developer creation sql statement
-	private static final String DATABASE_CREATE_DEVELOPER = 
-			"CREATE TABLE developer (" +
-					"developerid integer primary key autoincrement, " +
-					"name varchar(160), " +
-					"website varchar(200))";
-	
-	// Database pictures creation sql statement
-	private static final String DATABASE_CREATE_PICTURES = 
-			"CREATE TABLE pictures (" +
-					"pictureid integer primary key autoincrement, " +
-					"appid int(10), " +
-					"fileURL varchar(200))";
-	
-	// Database hardware creation sql statement
-	private static final String DATABASE_CREATE_HARDWARE = 
-			"CREATE TABLE hardware (" +
-					"hardwareid integer primary key autoincrement, " +
-					"name varchar(160), " +
-					"description varchar(200))";
-	
-	// Database platform creation sql statement
-	private static final String DATABASE_CREATE_PLATFORM = 
-			"CREATE TABLE platform (" +
-					"platformid integer primary key autoincrement, " +
-					"name varchar(160), " +
-					"description varchar(200)" +
-					"ramSize int(100)" +
-					"romSize int(100))";
-	
-	// Database platform creation sql statement
-	private static final String DATABASE_CREATE_BTDEVICES = 
-			"CREATE TABLE platform (" +
-					"btdeviceid integer primary key autoincrement, " +
-					"name varchar(160), " +
-					"mac-address varchar(200)" +
-					"installedApp int(10))";
-	
-	// Database appUsesPins creation sql statement
-		private static final String DATABASE_CREATE_APPUSESPINS = 
-				"CREATE TABLE appUsesPins (" +
-						"appid int(10) , " +
-						"hardwareid int(10)" +
-						"FOREIGN KEY (appid) REFERENCES app(appid)," +
-						"FOREIGN KEY (hardwareid) REFERENCES hardware(hardwareid))";
-	
 	public DatabaseHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		myContext = context;
 	}
 
 	// Method is called during creation of the database
 	@Override
 	public void onCreate(SQLiteDatabase database) {
-		database.execSQL(DATABASE_CREATE_APP);
-		database.execSQL(DATABASE_CREATE_VERSION);
-		database.execSQL(DATABASE_CREATE_RATINGS);
-		database.execSQL(DATABASE_CREATE_DEVELOPER);
-		database.execSQL(DATABASE_CREATE_PICTURES);
-		database.execSQL(DATABASE_CREATE_HARDWARE);
-		database.execSQL(DATABASE_CREATE_PLATFORM);
-		database.execSQL(DATABASE_CREATE_BTDEVICES);
-		database.execSQL(DATABASE_CREATE_APPUSESPINS);
+		SQLiteDatabase checkDB = null;
+
+		String myPath = DATABASE_PATH + DATABASE_NAME;
+		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+		if(checkDB.isOpen()){	//FIXME: dont know it this is correct. Tutorial said if(checkDB){
+			//do nothing
+		}else{
+			//create DB
+			database.execSQL(Constants.DATABASE_CREATE_APP);
+			database.execSQL(Constants.DATABASE_CREATE_VERSION);
+			database.execSQL(Constants.DATABASE_CREATE_RATINGS);
+			database.execSQL(Constants.DATABASE_CREATE_DEVELOPER);
+			database.execSQL(Constants.DATABASE_CREATE_PICTURES);
+			database.execSQL(Constants.DATABASE_CREATE_HARDWARE);
+			database.execSQL(Constants.DATABASE_CREATE_PLATFORM);
+			database.execSQL(Constants.DATABASE_CREATE_BTDEVICES);
+			database.execSQL(Constants.DATABASE_CREATE_APPUSESPINS);
+		}
 	}
 
-	// Method is called during an upgrade of the database,
+	//This method is called when database is upgraded like modifying the table structure, adding constraints to database etc
 	@Override
 	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
 		Log.d(DatabaseHelper.class.getName(), "Upgrading database from version " + 
@@ -119,4 +67,107 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 		onCreate(database);
 	}
+	
+	  /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase(){
+ 
+    	SQLiteDatabase checkDB = null;
+ 
+    	try{
+    		String myPath = DATABASE_PATH + DATABASE_NAME;
+    		checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+ 
+    	}catch(SQLiteException e){
+ 
+    		//database does't exist yet.
+ 
+    	}
+ 
+    	if(checkDB != null){
+ 
+    		checkDB.close();
+ 
+    	}
+ 
+    	return checkDB != null ? true : false;
+    }
+    
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transfering bytestream.
+     * */
+    private void copyDataBase() throws IOException{
+ 
+    	//Open your local db as the input stream
+    	InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
+ 
+    	// Path to the just created empty db
+    	String outFileName = DATABASE_PATH + DATABASE_NAME;
+ 
+    	//Open the empty db as the output stream
+    	OutputStream myOutput = new FileOutputStream(outFileName);
+ 
+    	//transfer bytes from the inputfile to the outputfile
+    	byte[] buffer = new byte[1024];
+    	int length;
+    	while ((length = myInput.read(buffer))>0){
+    		myOutput.write(buffer, 0, length);
+    	}
+ 
+    	//Close the streams
+    	myOutput.flush();
+    	myOutput.close();
+    	myInput.close();
+ 
+    }
+    
+    public void openDataBase() throws SQLException{
+    	 
+    	//Open the database
+        String myPath = DATABASE_PATH + DATABASE_NAME;
+    	myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+ 
+    }
+    
+    @Override
+  	public synchronized void close() {
+   
+      	    if(myDataBase != null)
+      		    myDataBase.close();
+   
+      	    super.close();
+   
+  	}
+    
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDataBase() throws IOException{
+ 
+    	boolean dbExist = checkDataBase();
+ 
+    	if(dbExist){
+    		//do nothing - database already exist
+    	}else{
+ 
+    		//By calling this method and empty database will be created into the default system path
+               //of your application so we are gonna be able to overwrite that database with our database.
+        	this.getReadableDatabase();
+ 
+        	try {
+ 
+    			copyDataBase();
+ 
+    		} catch (IOException e) {
+ 
+        		throw new Error("Error copying database");
+ 
+        	}
+    	}
+ 
+    }
 }
--- src/no/group09/database/MyDB.java
+++ src/no/group09/database/MyDB.java
@@ -1,46 +1,117 @@
 package no.group09.database;
 
+import java.io.IOException;
+
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class MyDB{  
 
 	private DatabaseHelper dbHelper;  
-
 	private SQLiteDatabase database;  
+	
+    public static final String PATH = "/data/data/no.group09.network/files/";
+    public static final String DB_SERVICES = "services.db";
+    public static final String DB_PROBES = "probes.db";
+    public static final String DB_NIC = "nic.db";
+    public static final String DB_SAVES = "saves.db";
 
-	public final static String EMP_TABLE="MyEmployees"; // name of table 
-
-	public final static String EMP_ID="_id"; // id value for employee
-	public final static String EMP_NAME="name";  // name of employee
 	/** 
 	 * 
 	 * @param context 
 	 */  
 	public MyDB(Context context){  
 		dbHelper = new DatabaseHelper(context);  
-		database = dbHelper.getWritableDatabase();  
+		database = dbHelper.getWritableDatabase(); 
+		
+	        dbHelper = new DatabaseHelper(context);
+	 
+	        try {
+	 
+	        	dbHelper.createDataBase();
+	 
+	 	} catch (IOException ioe) {
+	 
+	 		throw new Error("Unable to create database");
+	 
+	 	}
+	 
+	 	try {
+	 
+	 		dbHelper.openDataBase();
+	 
+	 	}catch(SQLException sqle){
+	 
+	 		throw sqle;
+	 
+	 	}
 	}
-
 
 	public long createRecords(String id, String name){  
 		ContentValues values = new ContentValues();  
-		values.put(EMP_ID, id);  
-		values.put(EMP_NAME, name);  
-		return database.insert(EMP_TABLE, null, values);  
+		values.put(Constants.APP_ID, id);  
+		values.put(Constants.APP_NAME, name);  
+		return database.insert(Constants.APP_TABLE, null, values);  
 	}    
 
-	public Cursor selectRecords() {
-		String[] cols = new String[] {EMP_ID, EMP_NAME};  
-		Cursor mCursor = database.query(true, EMP_TABLE,cols,null  
-				, null, null, null, null, null);  
-		if (mCursor != null) {  
-			mCursor.moveToFirst();  
+	/**
+	 * Selects all the records in the database
+	 * @return return a Cursor[] with all the values. Iterate over each cursor to get out the values.
+	 * Check for each Cursor[i] if its is null!
+	 */
+	public Cursor[] selectRecords() {
+		
+		Cursor mCursor[] = new Cursor[9];
 
+		//app(appid, name, description, developerid, icon) 
+		String[] app = new String[] {Constants.APP_ID, Constants.APP_NAME, Constants.APP_DESCRIPTION, Constants.APP_DEVELOPERID, Constants.APP_ICON};  
+		mCursor[1] = database.query(true, Constants.APP_TABLE, app, null, null, null, null, null, null); 
+		if (mCursor[1] != null) mCursor[1].moveToFirst();  
+		
+		//version(versionid, appid, version, fileURL, filesize)
+		String[] version = new String[] {Constants.VERSION_ID, Constants.VERSION_APPID, Constants.VERSION_VERSION, Constants.VERSION_FILEURL, Constants.VERSION_FILESIZE};
+		mCursor[2] = database.query(true, Constants.VERSION_TABLE, version, null, null, null, null, null, null); 
+		if (mCursor[2] != null) mCursor[2].moveToFirst();  
+		
+		//ratings(ratingid, versionid, title, rating)
+		String[] ratings = new String[] {Constants.RATINGS_ID, Constants.RATINGS_VERSIONID, Constants.RATINGS_TITLE, Constants.RATINGS_RATING};
+		mCursor[3] = database.query(true, Constants.RATINGS_TABLE, ratings, null, null, null, null, null, null); 
+		if (mCursor[3] != null) mCursor[3].moveToFirst();  
+		
+		//developer(developerid, name, website)
+		String[] developer = new String[] {Constants.DEVELOPER_ID, Constants.DEVELOPER_NAME, Constants.DEVELOPER_WEBSITE};
+		mCursor[4] = database.query(true, Constants.DEVELOPER_TABLE, developer, null, null, null, null, null, null); 
+		if (mCursor[4] != null) mCursor[4].moveToFirst(); 
+		
+		//pictures(pictureid, appid, fileURL)
+		String[] pictures = new String[] {Constants.PICTURES_ID, Constants.PICTURES_APPID, Constants.PICTURES_FILEURL};
+		mCursor[5] = database.query(true, Constants.PICTURES_TABLE, pictures, null, null, null, null, null, null); 
+		if (mCursor[5] != null) mCursor[5].moveToFirst();  		
+		
+		//hardware(hardwareid, name, description)
+		String[] hardware = new String[] {Constants.HARDWARE_ID, Constants.HARDWARE_NAME, Constants.HARDWARE_DESCRIPTION};
+		mCursor[6] = database.query(true, Constants.HARDWARE_TABLE, hardware, null, null, null, null, null, null); 
+		if (mCursor[6] != null) mCursor[6].moveToFirst();  
+		
+		//platform(platformid, name, description, ramSize, romSize)
+		String[] platform = new String[] {Constants.PLATFORM_ID, Constants.PLATFORM_NAME, Constants.PLATFORM_DESCRIPTION, Constants.PLATFORM_RAMSIZE, Constants.PLATFORM_ROMSIZE};
+		mCursor[7] = database.query(true, Constants.PLATFORM_TABLE, platform, null, null, null, null, null, null); 
+		if (mCursor[7] != null) mCursor[7].moveToFirst();  
+		
+		//btdevices(btdeviceid, name, mac-address, installedApp)
+		String[] btdevices = new String[] {Constants.BTDEVICES_ID, Constants.BTDEVICES_NAME, Constants.BTDEVICES_MACADDRESS, Constants.BTDEVICES_INSTALLEDAPP};
+		mCursor[8] = database.query(true, Constants.BTDEVICES_TABLE, btdevices, null, null, null, null, null, null); 
+		if (mCursor[8] != null) mCursor[8].moveToFirst();  		
+		
+		//appusespins(appid, hardwareid)
+		String[] appusespins = new String[] {Constants.APPUSESPINS_APPID, Constants.APPUSESPINS_HARDWAREID};
+		mCursor[9] = database.query(true, Constants.APPUSESPINS_TABLE, appusespins, null, null, null, null, null, null); 
+		if (mCursor[9] != null) mCursor[9].moveToFirst();  
 
-		}  
-		return mCursor; // iterate to get each value.
+		return mCursor;
 	}
 }
