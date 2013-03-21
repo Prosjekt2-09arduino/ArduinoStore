5eac798f33703b90c6932fdd37ccef59dc4f11ba
Wilhelm W Schive <wilumium@hotmail.com>
2013-03-21 12:49:51 +0100

    expanded javadoc for robotium. Attempt at making test but get error

--- test/src/no/group09/ucsoftwarestore/MainActivityTest.java
+++ test/src/no/group09/ucsoftwarestore/MainActivityTest.java
@@ -1,14 +1,25 @@
 package no.group09.ucsoftwarestore;
 
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+
+import no.group09.database.Save;
+import no.group09.database.entity.App;
 import no.group09.ucsoftwarestore.MainActivity;
 
 import com.jayway.android.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
 
 	// this is Robotium class used to support test cases that span over multiple activities.
 	private Solo solo; 
+	private ArrayList<App> allApps = new ArrayList<App>();
+	private Save save = new Save(getActivity().getBaseContext());
+
 
 	public MainActivityTest() {
 		super(MainActivity.class);
@@ -21,34 +32,76 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		solo = new Solo(getInstrumentation(), getActivity());
 	}
 
-	@Override
-	public void tearDown() throws Exception {
-		solo.finishOpenedActivities();
-	}
-	
+
+
 	public void testSettings() throws Exception{
-<<<<<<< HEAD:test/src/no/group09/ucsoftwarestore/MainActivityTest.java
 		solo.clickOnActionBarItem(R.id.settings);
-<<<<<<< HEAD
-		
+
 	}
-	public void testHideShowCompatible() throws Exception{
+	public void testPopulateDB() throws Exception{
+
+		save.populateDatabase();
+		allApps = save.getAllApps();
+
+		Assert.assertEquals(15, allApps.size());
+
+	}
+	/**
+	 * Goes through all filtration options to ensure proper filtration.
+	 * @throws Exception
+	 */
+	public void testTotalShopFiltration() throws Exception{
+
+		ArrayList<App> allGames = new ArrayList<App>();
+		ArrayList<App> allMedical = new ArrayList<App>();
+		ArrayList<App> allTools = new ArrayList<App>();
+		ArrayList<App> allMedia = new ArrayList<App>();
+		ArrayList<App> allCompatible = new ArrayList<App>();
+
+
+		for (App app : allApps) {
+
+			String category = app.getCategory();
+
+			if(category.equalsIgnoreCase("games")){
+				allGames.add(app);
+			}
+			else if(category.equalsIgnoreCase("medical")){
+				allMedical.add(app);
+			}
+			else if(category.equalsIgnoreCase("tools")){
+				allTools.add(app);
+			}
+			else if(category.equalsIgnoreCase("media")){
+				allMedia.add(app);
+			}
+
+			if(save.getRequirementsByID(app.getRequirementID()).isCompatible()){
+				allCompatible.add(app);
+			}
+			solo.clickOnText("Games");
+
+			ArrayList<View> currentViews = solo.getViews();
+			Log.e("mainactivity currentViews Size", String.valueOf(currentViews.size()));
+
+		}
+
+
 		/**
 		 * check what apps are shown, and if they are compatible or not
 		 * open action overflow
 		 * click hide/show compatible
 		 * check if the correct apps are hidden
 		 */
-	}
-	//	Test all filtrations, games, medical, etc. try different variations in order to try to bug it out.
-//		Have crashed the app from the category view, not sure what happened.
-=======
 
-=======
-		solo.clickOnMenuItem("Settings");
-		solo.
->>>>>>> Searching now works and returns a toast.:test/src/no/group09/ucsoftwarestore/test/MainActivityTest.java
+		//	Test all filtrations, games, medical, etc. try different variations in order to try to bug it out.
+		//		Have crashed the app from the category view, not sure what happened.		
+
 	}
-	
->>>>>>> master
+	@Override
+	public void tearDown() throws Exception {
+		solo.finishOpenedActivities();
+	}
+
 }
+
